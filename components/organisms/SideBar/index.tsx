5843b037352ab30eda3ffb668feb70c5ef47af34
Rahadian Reza Rizaldy <59022153+apaajaa22@users.noreply.github.com>
2021-09-22 17:39:57 +0700

    refactor done, add storybook

--- components/organisms/SideBar/index.tsx
+++ components/organisms/SideBar/index.tsx
@@ -1,20 +1,24 @@
 import Footer from './Footer';
-import MenuItem from './Menuitem';
-import Profile from './profile';
+import MenuItem from './MenuItem';
+import Profile from './Profile';
 
-export default function SideBar() {
+interface SideBarProps {
+  activeMenu: 'overview' | 'transactions' | 'settings'
+}
+export default function SideBar(props: SideBarProps) {
+  const { activeMenu } = props;
   return (
     <section className="sidebar">
       <div className="content pt-50 pb-30 ps-30">
         <Profile />
         <div className="menus">
-          <MenuItem active title="Overview" icon="ic-menu-overview" />
-          <MenuItem title="Transactions" icon="ic-menu-transaction" />
-          <MenuItem title="Messages" icon="ic-menu-messages" />
-          <MenuItem title="Card" icon="ic-menu-card" />
-          <MenuItem title="Rewards" icon="ic-menu-reward" />
-          <MenuItem title="Settings" icon="ic-menu-setting" />
-          <MenuItem title="Logout" icon="ic-menu-logout" />
+          <MenuItem title="Overview" icon="ic-menu-overview" href="/member" active={activeMenu === 'overview'} />
+          <MenuItem title="Transactions" icon="ic-menu-transaction" href="/member/transactions" active={activeMenu === 'transactions'} />
+          <MenuItem title="Messages" icon="ic-menu-messages" href="/member" />
+          <MenuItem title="Card" icon="ic-menu-card" href="/member" />
+          <MenuItem title="Rewards" icon="ic-menu-reward" href="/member" />
+          <MenuItem title="Settings" icon="ic-menu-setting" href="/member/edit-profile" active={activeMenu === 'settings'} />
+          <MenuItem title="Logout" icon="ic-menu-logout" href="/sign-in" />
         </div>
         <Footer />
       </div>
